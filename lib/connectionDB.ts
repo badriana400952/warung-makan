0706e26b4bd4a7d18450b58bb35e51d27f1c7495
badriana400952 <badriana0402@gmail.com>
2025-08-20 16:21:06 +0700

    comen

--- lib/connectionDB.ts
+++ lib/connectionDB.ts
@@ -1,6 +1,15 @@
-import { Pool } from 'pg';
+// import { Pool } from 'pg';
 
+// export const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: { rejectUnauthorized: false }
+// });
+
+import { Pool } from 'pg';
 export const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  user: 'postgres',
+  host: 'localhost',
+  database: 'warungMakan',
+  password: '123456789',
+  port: 5432,
 });
