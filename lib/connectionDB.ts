53b7f3bbb80f9253a428f46990c926337be6161c
badriana400952 <badriana0402@gmail.com>
2025-08-20 14:32:49 +0700

    pool

--- lib/connectionDB.ts
+++ lib/connectionDB.ts
@@ -1,8 +1,6 @@
 import { Pool } from 'pg';
+
 export const pool = new Pool({
-  user: process.env.NEXT_PUBLIC_PGUSER,
-  host: process.env.NEXT_PUBLIC_PGHOST,
-  database: process.env.NEXT_PUBLIC_PGDATABASE,
-  password: process.env.NEXT_PUBLIC_PGPASSWORD,
-  port: Number(process.env.NEXT_PUBLIC_PGPORT),
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
